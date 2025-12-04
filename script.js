ebcf7c76b3fa52832f0d9c4bfcb9bcf48782a861
evgeniy zimagorov <zimagorov.evgeniy@gmail.com>
2025-12-04 12:33:02 +0300

    feat: add initial page layout

---
+++ script.js
@@ -0,0 +1,6 @@
+// const gridItem = document.querySelector(".grid-gallery");
+
+// gridItem.addEventListener('mouseover', function(event) {
+//     console.log(event);
+// })
+// console.log(gridItem);
